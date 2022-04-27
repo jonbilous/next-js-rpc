b63e2403e46ed81be2befb448388699af521510c
Jon Bilous <jon@jonbilous.com>
2022-04-27 14:10:17 -0400

    fix(ssrconsumer): copy props before mutating

--- src/client/ssrConsumer.tsx
+++ src/client/ssrConsumer.tsx
@@ -5,13 +5,15 @@ const ssrConsumer = <T extends Record<string, unknown>>(
   Component: React.FC<T>
 ): React.FC<T> => {
   return (props) => {
+    const parsedProps = { ...props };
+
     Object.entries(props).forEach(([key, value]: [string, any]) => {
       if (value.json && value.meta) {
-        props[key as keyof T] = superjson.deserialize(value);
+        parsedProps[key as keyof T] = superjson.deserialize(value);
       }
     });
 
-    return <Component {...props} />;
+    return <Component {...parsedProps} />;
   };
 };
 
