a765fbc39f0ebaf0df3c0630ec87b3763094933f
Jon Bilous <jon@jonbilous.com>
2022-04-27 10:19:41 -0400

    feat(error handling): added HTTPError class

--- src/server/index.test.ts
+++ src/server/index.test.ts
@@ -1,7 +1,17 @@
 import { createMocks } from "node-mocks-http";
 import { describe, expect, test, vi } from "vitest";
-import { createApi } from ".";
-import zod from "zod";
+import { createApi, HTTPError } from ".";
+import zod, { ZodDate } from "zod";
+import { CacheProvider } from "types";
+
+const cacheProvider: CacheProvider = {
+  defaultTtl: 5000,
+  get: async <T extends unknown>(key: string) => {
+    return null as T;
+  },
+  write: async (key) => {},
+  flush: async (key) => {},
+};
 
 describe("test createHandler", () => {
   test("context works", async () => {
@@ -14,14 +24,7 @@ describe("test createHandler", () => {
     });
 
     const { createHandler } = createApi({
-      cacheProvider: {
-        defaultTtl: 5000,
-        get: async <T extends unknown>(key: string) => {
-          return "z" as T;
-        },
-        write: async (key) => {},
-        flush: async (key) => {},
-      },
+      cacheProvider,
     });
 
     const { req, res } = createMocks({ method: "POST" });
@@ -44,5 +47,121 @@ describe("test createHandler", () => {
     const result = JSON.parse(res._getData());
 
     expect(result).toBe(testValue);
+  });
+
+  test("httperror works in handler", async () => {
+    const code = 404;
+    const error = "it does not work!";
+
+    const { createHandler } = createApi({
+      cacheProvider,
+    });
+
+    const { req, res } = createMocks({ method: "POST" });
+
+    const handler = createHandler({
+      url: "/hello",
+      fn: async (data, ctx) => {
+        throw new HTTPError(error, code);
+      },
+
+      schema: zod.any(),
+    });
+
+    await handler(req, res);
+
+    const result = JSON.parse(res._getData());
+
+    expect(res.statusCode).toBe(code);
+    expect(result).toHaveProperty("error");
+    expect(result.error).toBe(error);
+  });
+
+  test("httperror works in ctx", async () => {
+    const code = 404;
+    const error = "it does not work!";
+
+    const { createHandler } = createApi({
+      cacheProvider,
+    });
+
+    const { req, res } = createMocks({ method: "POST" });
+
+    const handler = createHandler({
+      url: "/hello",
+      fn: async (data, ctx) => {
+        return { hello: "world" };
+      },
+      ctx: {
+        user: async () => {
+          throw new HTTPError(error, code);
+        },
+      },
+      schema: zod.any(),
+    });
+
+    await handler(req, res);
+
+    const result = JSON.parse(res._getData());
+
+    expect(res.statusCode).toBe(code);
+    expect(result).toHaveProperty("error");
+    expect(result.error).toBe(error);
+  });
+
+  test("error returns 501", async () => {
+    const { createHandler } = createApi({ cacheProvider });
+
+    const { req, res } = createMocks({ method: "POST" });
+
+    const handler = createHandler({
+      url: "/hello",
+      fn: async (data, ctx) => {
+        return { hello: "world" };
+      },
+      ctx: {
+        user: async () => {
+          throw new Error("!!!!!");
+        },
+      },
+      schema: zod.any(),
+    });
+
+    await handler(req, res);
+
+    const result = JSON.parse(res._getData());
+
+    expect(res.statusCode).toBe(501);
+
+    expect(result).toHaveProperty("error");
+
+    expect(result.error).toBe("Internal server error");
+  });
+
+  test("schema validation", async () => {
+    const { createHandler } = createApi({ cacheProvider });
+
+    const { req, res } = createMocks({
+      method: "POST",
+      body: { mello: "world" },
+    });
+
+    const handler = createHandler({
+      url: "/hello",
+      fn: async (data, ctx) => {
+        return data;
+      },
+      schema: zod.object({ hello: zod.string() }),
+    });
+
+    await handler(req, res);
+
+    const result = JSON.parse(res._getData());
+
+    expect(res.statusCode).toBe(404);
+
+    expect(result).toHaveProperty("error");
+
+    expect(result.error).toBe("Input validation error");
   });
 });
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,18 @@ import { NextApiResponse } from "next";
 import type {
   ApiRequest,
   CacheProvider,
+  ContextResult,
   HandlerContext,
   HandlerDefinition,
 } from "../types";
 import zod from "zod";
+import superjson from "superjson";
+
+export class HTTPError extends Error {
+  constructor(public message: string, public status: number) {
+    super(message);
+  }
+}
 
 export const createApi = ({
   cacheProvider,
@@ -24,88 +32,51 @@ export const createApi = ({
     schema,
     ctx,
     cache,
+    url,
   }: HandlerDefinition<RequestBody, ResponseType, Ctx, Url>) => {
-    const handler = async (
-      req: ApiRequest<RequestBody, Url>,
-      res: NextApiResponse<ResponseType>
-    ) => {
+    const validate = (data: RequestBody) => {
       try {
-        if (req.method !== "POST") {
-          return res.status(400).json({ error: "Must use POST" } as any);
-        }
-
-        const validated = schema
-          ? schema.parse(req.body)
-          : zod.any().parse(req.body);
-
-        const contextResult = {} as Record<keyof Ctx, any>;
-
-        await Promise.all(
-          Object.entries(ctx || {}).map(async ([key, fn]) => {
-            const result = await fn({ req, res });
-
-            contextResult[key as keyof Ctx] = result;
-          })
-        );
-
-        const handlerCtx = { req, res, ...contextResult };
-
-        if (cache) {
-          const key = cache.getKey(validated, handlerCtx);
-
-          const cachedResult = await cacheProvider.get<ResponseType>(key);
-
-          if (cachedResult) {
-            return res.status(200).json(cachedResult);
-          }
-        }
-
-        return fn(validated, handlerCtx)
-          .then((result) => {
-            if (cache) {
-              const key = cache.getKey(validated, handlerCtx);
-
-              cacheProvider.write(
-                key,
-                result,
-                cache.ttl ?? cacheProvider.defaultTtl
-              );
-            }
-
-            return res.status(200).json(result);
-          })
-          .catch((err) => res.status(501).end());
+        return schema ? schema.parse(data) : zod.any().parse(data);
       } catch (err) {
-        return res.status(400).json({ error: "Error" } as any);
+        throw new HTTPError("Input validation error", 404);
       }
     };
 
-    const serverFn = async (
-      data: RequestBody,
-      { req, res }: HandlerContext
-    ) => {
-      const validated = schema ? schema.parse(data) : zod.any().parse(data);
-
+    const buildContext = async ({ req, res }: HandlerContext) => {
       const contextResult = {} as Record<keyof Ctx, any>;
 
       await Promise.all(
-        Object.entries(ctx || {}).map(async ([key, ctxFn]) => {
-          const result = await ctxFn({ req, res });
+        Object.entries(ctx || {}).map(async ([key, fn]) => {
+          const result = await fn({ req, res });
+
           contextResult[key as keyof Ctx] = result;
         })
       );
 
-      const handlerCtx = { ...contextResult, req, res };
+      return { req, res, ...contextResult };
+    };
+
+    const getResult = async (
+      data: RequestBody,
+      ctx: HandlerContext,
+      fn: (
+        data: RequestBody,
+        ctx: HandlerContext<ContextResult<Ctx>>
+      ) => Promise<ResponseType>
+    ) => {
+      const validatedData = validate(data);
+
+      const handlerCtx = await buildContext({ req: ctx.req, res: ctx.res });
 
       if (cache) {
-        const key = cache.getKey(validated, handlerCtx);
+        const key = [url, cache.getKey(validatedData, handlerCtx)].join("-");
 
         const cachedResult = await cacheProvider.get<ResponseType>(key);
 
         if (cachedResult) {
           return cachedResult;
         } else {
-          const result = await fn(validated, handlerCtx);
+          const result = await fn(validatedData, handlerCtx);
 
           cacheProvider.write(
             key,
@@ -117,7 +88,37 @@ export const createApi = ({
         }
       }
 
-      return fn(validated, handlerCtx);
+      return fn(validatedData, handlerCtx);
+    };
+
+    const handler = async (
+      req: ApiRequest<RequestBody, Url>,
+      res: NextApiResponse<ResponseType>
+    ) => {
+      if (req.method !== "POST") {
+        return res.status(400).json({ error: "Must use POST" } as any);
+      }
+
+      return getResult(req.body, { req, res }, fn)
+        .then((result) => {
+          return res.status(200).json(result);
+        })
+        .catch((err) => {
+          if (err instanceof HTTPError) {
+            return res.status(err.status).json({ error: err.message } as any);
+          }
+
+          return res
+            .status(501)
+            .json({ error: "Internal server error" } as any);
+        });
+    };
+
+    const serverFn = async (
+      data: RequestBody,
+      { req, res }: HandlerContext
+    ) => {
+      return getResult(data, { req, res }, fn);
     };
 
     handler.ssr = serverFn;
